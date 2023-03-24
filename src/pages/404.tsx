a208c36395920a1b485045d53950af95a4a0cce1
Alexander Kochemasov <akochemasov@gmail.com>
2023-03-24 15:38:58 +0100

    Заголовок для страницы

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Layout from "@/components/layouts/Layout";
 import Image from "next/image";
+import Head from "next/head";
 
 import Image404 from "../../public/404.png";
 
 const NotFound = () => {
   return (
     <Layout>
+      <Head>
+        <title>Not found</title>
+      </Head>
       <h2>Not found</h2>
       <Image src={"/404.png"} alt={"404"} width={450} height={433} />
 
