a7e0feaf943e8b2c74f76ba89ccf30de74db34f1
Alexander Kochemasov <akochemasov@gmail.com>
2023-03-24 14:34:37 +0100

    Подключение картинки на страницу

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Layout from "@/components/layouts/Layout";
+import Image from "next/image";
+
+import Image404 from "../../public/404.png";
 
 const NotFound = () => {
-  return <>Not found</>;
+  return (
+    <Layout>
+      <h2>Not found</h2>
+      <Image src={"/404.png"} alt={"404"} width={450} height={433} />
+
+      {/* растянуть на всю страницу */}
+      {/*<Image src={"/404.png"} alt={"404"} fill />*/}
+
+      {/* размер не обязательно указывать */}
+      {/*<Image src={Image404} alt={"404"} />*/}
+    </Layout>
+  );
 };
 
 export default NotFound;
