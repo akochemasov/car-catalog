5b546ef0df0fda4dc1bdb2462fe1d52c8fa0395a
Alexander Kochemasov <akochemasov@gmail.com>
2023-03-25 12:06:13 +0100

    SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import HomeScreen from "@/components/screens/home/HomeScreen";
-import { GetServerSideProps, NextPage } from "next";
+import { GetServerSideProps, GetStaticProps, NextPage } from "next";
 import { ICarData } from "@/types/car.type";
 import { CarsService } from "@/services/cars.service";
 
@@ -13,12 +13,26 @@ const HomePage: NextPage<ICarData> = ({ cars }) => {
 
 //fetching data
 //SSR - принимает дженерик
-export const getServerSideProps: GetServerSideProps<ICarData> = async () => {
+// export const getServerSideProps: GetServerSideProps<ICarData> = async () => {
+//   const cars = await CarsService.getAll();
+//
+//   return {
+//     //обязательно обернуть в объект
+//     props: { cars },
+//     // notFound,
+//     // redirect,
+//   };
+// };
+
+//SSG
+//локально работает аналогично SSR
+export const getStaticProps: GetStaticProps<ICarData> = async () => {
   const cars = await CarsService.getAll();
 
   return {
     //обязательно обернуть в объект
     props: { cars },
+    revalidate: 60, //ревалидация данных
     // notFound,
     // redirect,
   };
